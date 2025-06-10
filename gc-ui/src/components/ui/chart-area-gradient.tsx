626b3b637546904e9f917fb8d3a19479b0ecee06
Sebastian Frick <sebastian@monkeysquad.de>
2025-06-10 09:06:49 +0200

    [wip]: optimizing samples

--- gc-ui/src/components/ui/chart-area-gradient.tsx
+++ gc-ui/src/components/ui/chart-area-gradient.tsx
@@ -19,6 +19,9 @@ import {
 } from "@/components/ui/chart";
 import { useEffect, useState } from "react";
 import type { WorkoutSample } from "@/App";
+import { sampleData } from "@/lib/sampler";
+import { sma } from "@/lib/sma";
+import { createSMA, SMA } from "downsample";
 
 export const description = "An area chart with gradient fill";
 
@@ -39,6 +42,9 @@ const samples = [
 ];
 
 const chartConfig = {
+  y: {
+    label: "Herzfrequenz",
+  },
   desktop: {
     label: "Desktop",
     color: "var(--chart-1)",
@@ -54,50 +60,88 @@ type ChartAreaGradientProps = {
   workout: WorkoutSample[];
 };
 
+function mergeData(data: { [k: string]: Array<{ x: number; y: number; }> }) {
+  console.time('merging data')
+  const seenX = new Set<number>();
+  const result = [];
+  for (const key in data) {
+    for (const p in data[key]) {
+      const point = data[key][p];
+      if (point.x && !seenX.has(point.x)) {
+        let collector: { [k: string]: number } = { x: point.x };
+        for (const k in data) {
+          collector[k] = data[k][p].y
+        }
+        result.push(collector);
+        seenX.add(point.x);
+      } else if (point.x && seenX.has(point.x)) {
+        result.filter(p => p.x === point.x)[0][key] = point.y;
+      } else {
+        console.warn('No point.x found for:', point);
+      }
+    }
+  }
+  const sorted = result.sort((a, b) => a.x - b.x);
+  console.timeEnd('merging data')
+  return sorted;
+}
+
 export function ChartAreaGradient({
   workout,
   workoutId,
 }: ChartAreaGradientProps) {
-  // const [workout, setWorkout] = useState<any[]>([]);
   const [error, setError] = useState<string | null>(null);
-  console.log("render with workoutId:", workoutId);
-  console.log("render with workout:", workout);
+  const sampleSize = workout.length;
+  const downsampled = sampleData(workout, 2000, ["cadence", "heart_rate", "power"]);
+  const merged = mergeData(downsampled);
+  console.log('merged data',);
+
+  // console.log("render with downsampled:", downsampled);
+  // const ma = SMA(downsampled.heart_rate, 60);
+  // console.log("render with ma:", ma.prototype);
+
+  // console.log("render with workoutId:", workoutId);
+  // console.log("render with workout:", workout);
+  // console.log("render with ma:", ma);
 
   return (
     <Card>
       <CardHeader>
         <CardTitle>Bemer Cyclassics</CardTitle>
       </CardHeader>
+      <CardDescription>Samples: {sampleSize}, Downsampled: {merged.length}</CardDescription>
       <CardContent>
         <ChartContainer config={chartConfig}>
           <AreaChart
             accessibilityLayer
-            data={workout}
+            // data={workout.map(e => ({ x: e.timestamp, y: e.heart_rate }))}
+            data={merged}
+            // data={downsampled.cadence}
             margin={{
               left: 12,
               right: 12,
             }}
           >
-            <CartesianGrid vertical={false} />
+            <CartesianGrid vertical={true} />
             <XAxis
-              dataKey="timestamp"
+              dataKey="x"
               tickLine={false}
               axisLine={false}
               tickMargin={8}
-              // tickFormatter={(value) => value.slice(0, 3)}
+            // tickFormatter={(value) => value.slice(0, 3)}
             />
-            <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+            <ChartTooltip cursor={true} content={<ChartTooltipContent indicator="line" />} />
             <defs>
               <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
                 <stop
-                  offset="5%"
+                  offset="10%"
                   stopColor="var(--color-desktop)"
-                  stopOpacity={0.8}
+                  stopOpacity={0.6}
                 />
                 <stop
-                  offset="95%"
+                  offset="90%"
                   stopColor="var(--color-desktop)"
-                  stopOpacity={0.1}
+                  stopOpacity={0.01}
                 />
               </linearGradient>
               <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
@@ -125,7 +169,7 @@ export function ChartAreaGradient({
               dataKey="heart_rate"
               type="natural"
               fill="url(#fillDesktop)"
-              fillOpacity={0.4}
+              fillOpacity={0.5}
               stroke="var(--color-desktop)"
               stackId="a"
             />
