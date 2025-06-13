28be72ef5206774896f2aa86a53db7d4b70fed9e
Sebastian Frick <sebastian@monkeysquad.de>
2025-06-14 00:52:04 +0200

    chore: Start script

--- start.sh
+++ start.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-mvn spring-boot:run -pl gc-main
+case "$1" in
+"main")
+  echo "Starte GC Main..."
+  mvn spring-boot:run -pl gc-main
+  ;;
+"ui")
+  echo "Starte GC UI..."
+  npm run dev --prefix gc-ui
+  ;;
+*)
+  echo "Verwendung: $0 {main|ui}"
+  exit 1
+  ;;
+esac
